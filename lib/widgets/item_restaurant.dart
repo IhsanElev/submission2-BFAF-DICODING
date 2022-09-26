32cb938ad4e662fa4e3c4756b0321168ab15ca60
IhsanElev <miftahulihsan7881@gmail.com>
2022-09-26 22:34:14 +0700

    perbaiki searching

--- lib/widgets/item_restaurant.dart
+++ lib/widgets/item_restaurant.dart
@@ -13,57 +13,59 @@ class ItemRestaurant extends StatelessWidget {
     double rating = restaurant.rating.toDouble();
     return Card(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: InkWell(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-                vertical: AppLayout.getHeight(12),
-                horizontal: AppLayout.getWidth(12)),
-            child: ListTile(
-              contentPadding: EdgeInsets.symmetric(
-                  vertical: AppLayout.getHeight(8),
-                  horizontal: AppLayout.getWidth(8)),
-              leading: ConstrainedBox(
-                constraints: BoxConstraints(
-                  minWidth: AppLayout.getWidth(80),
-                  minHeight: AppLayout.getHeight(80),
-                  maxWidth: AppLayout.getWidth(80),
-                  maxHeight: AppLayout.getHeight(80),
-                ),
-                child: Hero(
-                    tag: restaurant.pictureId,
-                    child: Image.network(ApiService().urlImage+restaurant.pictureId),),
-              ),
-              title: Text(restaurant.name),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      const Icon(Icons.location_city),
-                      Text(restaurant.city),
-                    ],
+      child: Material(
+        child: InkWell(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                  vertical: AppLayout.getHeight(12),
+                  horizontal: AppLayout.getWidth(12)),
+              child: ListTile(
+                contentPadding: EdgeInsets.symmetric(
+                    vertical: AppLayout.getHeight(8),
+                    horizontal: AppLayout.getWidth(8)),
+                leading: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minWidth: AppLayout.getWidth(80),
+                    minHeight: AppLayout.getHeight(80),
+                    maxWidth: AppLayout.getWidth(80),
+                    maxHeight: AppLayout.getHeight(80),
                   ),
-                  Row(
-                    children: [
-                      Text(rating.toString()),
-                      const SizedBox(),
-                      RatingBarIndicator(
-                        rating: rating,
-                        itemBuilder: (context, index) => const Icon(
-                          Icons.star,
-                          color: Colors.amber,
+                  child: Hero(
+                      tag: restaurant.pictureId,
+                      child: Image.network(ApiService().urlImage+restaurant.pictureId),),
+                ),
+                title: Text(restaurant.name),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(Icons.location_city),
+                        Text(restaurant.city),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(rating.toString()),
+                        const SizedBox(),
+                        RatingBarIndicator(
+                          rating: rating,
+                          itemBuilder: (context, index) => const Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                          ),
+                          itemCount: 5,
+                          direction: Axis.horizontal,
+                          itemSize: 20,
                         ),
-                        itemCount: 5,
-                        direction: Axis.horizontal,
-                        itemSize: 20,
-                      ),
-                    ],
-                  )
-                ],
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          onTap: ()=> Get.to(DetailScreen(restaurant: restaurant))),
+            onTap: ()=> Get.to(DetailScreen(restaurant: restaurant))),
+      ),
     );
   }
 }
